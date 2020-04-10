6c0235c83185c00fb2a6ec864547ae8f3677559a
roberto <robertoweller78@gmail.com>
2020-04-10 11:30:42 -0300

    Esse é um jogo teste modo história, está em desenvolvimento aberto a ajuda. Ficarei grato a todos que ajudarem e darei devido crédito.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Necessidades para o jogo, precios de:
+
+* Imagens de fundo (tenho alguns exemplos)
+* Saber como dar o tempo de adicionar cada Label
+* Definir qual paleta de cores será usadas
+
+-------------------------------------
+# Idéia inicial 
+* Por um fundo bonito
+* Por caixas de texto texturizadas, que serão adicionadas a cada 3 segundos?
+
+* Outra idéia é criar um calculo dependendo do tamanho do texto para adicionar cada caixa dependendo
+do tamanho do texto
+--------------------------------------
+
+# Caracteristicas do jogo:
+
+* Jogo será para meninas?
+* - Jogo tentará abranger ambos o público,
+* - mas acredito que pode só um tipo de público poderá gostar mais.
+* Terá cores vivas ?
+* - Agora só consigo pensar em cores azul, preto, branco e rosa.(para tentar atingir vários públicos)
+
+* - Pode-se até dar ao usuário opções de cores que lhe agrada mais.
+
+* Nome do jogo: PerdiTuBes
+
+* Missão: Aumentar seu clã de fãns e ficar mais próximo do seu(sua) artista
---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.boxlayout import BoxLayout
+
+
+'''
+Ler sober o jogo no arquivo README.md
+'''
+
+
+class Mensagem(ScrollView):
+    def __init__(self, mensagem, **kwargs):
+        super().__init__(**kwargs)
+        # A classe incial estará esperando a classe mensagem como argumento
+        # Dentor de mensagem ficara o conteudo daclasse Conteudo e será adicionado o widget da mensagem.
+        for mensagem in mensagem:
+            self.ids.men.add_widget(Conteudo(text=mensagem))
+
+
+class Conteudo(BoxLayout):
+    def __init__(self, text='', **kwargs):
+        super().__init__(**kwargs)
+        # e o conteudo espera text como arguemnto que é passado na classe mensagem
+        # se não for passado nada, terá texto vazio ''
+        self.ids.lab.text = text
+
+
+class PerdiTubes(App):
+    def build(self):
+        return Mensagem(['Testando', 'Aqui terá uma mensagem!', 'Esse é um texto \nque passa a caixa \nde mensagem espero poder \nresolver logo isso'])
+
+
+if __name__ == '__main__':
+    PerdiTubes().run()
