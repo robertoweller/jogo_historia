6a9058469721627eecd3274a321f4259699fe019
roberto <robertoweller78@gmail.com>
2020-04-13 01:17:22 -0300

    Tempo ao adicionar cada balão. Comentando o código melhor.

--- main.py
+++ main.py
@@ -3,30 +3,35 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 
-
 '''
-Ler sober o jogo no arquivo README.md
+Ler sobre o jogo no arquivo README.md
 '''
 
 
 class Mensagem(ScrollView):
     def __init__(self, mensagem, **kwargs):
         super().__init__(**kwargs)
+        # A classe incial estará esperando a classe mensagem como argumento
+        # Essa é aquela mensagem da lista da classe PerdiTubes
         self.mensagem = mensagem
         self.todas_mensagnes = len(mensagem)
         self.conta = 0
-        # Adicionar uma lista para inverter?
-        # A classe incial estará esperando a classe mensagem como argumento
-        # Dentro de mensagem ficara o conteudo daclasse Conteudo e será adicionado o widget da mensagem.
+
+        # O Clock atende o que eu quero, que é adicionar uma mensagem, depois de 4 segundo adiciona outra.
         Clock.schedule_interval(self.chama_mensagem, 4)
+
+        # O for abaixo adicionava tudo de uma fez só
         # for mensagem in mensagem:
         #     self.ids.men.add_widget(Conteudo(text=mensagem))
 
         # Vai adicionar uma a uma as mensagens
 
     def chama_mensagem(self, sla):
+        # Quando acabar as mensagens vai parar de adicionar os widgets, isso é para não dar erro.
         if self.conta < self.todas_mensagnes:
             print(self.mensagem[self.conta])
+
+            # Dentro de mensagem ficará o conteudo daclasse Conteudo e será adicionado o widget da mensagem.
             self.ids.men.add_widget(Conteudo(text=self.mensagem[self.conta]))
             self.conta += 1
 
