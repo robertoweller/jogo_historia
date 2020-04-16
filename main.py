55446d52e41b55339aeb345e7174f9e2c6287558
roberto <robertoweller78@gmail.com>
2020-04-16 11:45:54 -0300

    Balões adicionados, balão azul e rosa. E tem a como salvar as conversa que vocẽ escreve, fica salva no documento conversas.txt

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-
-'''ize_hint_y:.1
-        size_hint_x:.1 jogo no arquivo README.md
+import os
+'''
+       Leia maus sobre o jogo no arquivo README.md
 '''
 
 
@@ -18,6 +16,8 @@ class Mensagem(BoxLayout):
         self.todas_mensagnes = len(mensagem)
         self.conta = 0
         self.fala = []
+        self.conversadas = []
+        self.primeira = True
 
         # O Clock atende o que eu quero, que é adicionar uma mensagem, depois de 4 segundo adiciona outra.
         Clock.schedule_interval(self.chama_mensagem, 4)
@@ -26,8 +26,7 @@ class Mensagem(BoxLayout):
         # for mensagem in mensagem:
         #     self.ids.men.add_widget(Conteudo(text=mensagem))
 
-        # Vai adicionar uma a uma as mensagens
-
+    # Vai adicionar uma a uma as mensagens
     def chama_mensagem(self, sla):
 
         # Quando acabar as mensagens vai parar de adicionar os widgets, isso é para não dar erro.
@@ -40,16 +39,9 @@ class Mensagem(BoxLayout):
 
             # Procura dentro da biblioteca quem está falando e adicina o widget
             self.ids.men.add_widget(falas[self.mensagem[self.conta][:4]])
-            # Testando achar uma formade achar a posição dos elementos e mudar
-            # self.ids.posi.scroll_y = 1.0
-            print(self.ids.posi.scroll_x)
-            # Distancia de de rolagem em pixels
-            # print(self.ids.posi.convert_distance_to_scroll('scroll_x', 'scroll_y'))
-            # Mostrar o posicionamento das margens
-            print(self.ids.posi.bar_margin)
-            # Nesse print mostra quem está falando no momento
-            # Apague o [:4] para ver a conversa toda no print
-            print(self.mensagem[self.conta][:4])
+
+            # Mostra a pessoa, se decomentar o [:4] mostra a conversa toda junto com a pessoa
+            # print(self.mensagem[self.conta][:4])
             self.conta += 1
 
     def conversa_digitada(self):
@@ -63,8 +55,27 @@ class Mensagem(BoxLayout):
 
         # Escrever no terminal a conversa que stá acontecendo
         print(mifala)
+        self.conversadas.append(mifala)
         # Limpa o texto da caixa e deixa o @eeu, coloquei aqui o personagem que vai aparece mais, para facilitar
         self.ids.mifala.text = '@euu '
+
+    def salva(self):
+        if self.primeira:
+            print(f'{self.mensagem}')
+            cov = f'{self.mensagem}'
+            if str(self.mensagem) != '[]':
+                # Mensagem salva no documento conversas.txt
+                os.system(f'echo "{cov}" > conversas.txt')
+            if str(self.conversadas) != '[]':
+                # print(self.conversadas)
+                cov = f'{self.mensagem}\n{self.conversadas}'
+                os.system(f'echo "{cov}" > conversas.txt')
+            self.primeira = False
+        else:
+            if str(self.conversadas) != '[]':
+                cov = f'{self.mensagem}\n{self.conversadas}'
+                os.system(f'echo "{cov}" > conversas.txt')
+                # print(self.conversadas)
 
 
 class CaylaFala(BoxLayout):
@@ -86,10 +97,7 @@ class PerdiTubes(App):
     def build(self):
         # Coloque @euu antes da frase para o balão ser adicionado ao seu lado + sua imagem
         # Ou coloque @cay antes para o balão ser adicionado do lado outra pessoa + a imagem da pessoa
-        return Mensagem(['@cay oi hihi', '@euu olá :)', '@cay quero pão de queijo', '@euu olá :)', '@euu olá :)',
-                         '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)'
-                         , '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)'
-                         , '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)', '@euu olá :)'])
+        return Mensagem(['@cay oi hihi', '@euu olá :)'])
 
 
 if __name__ == '__main__':
