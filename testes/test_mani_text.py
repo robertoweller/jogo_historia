41c6c34b1de2200c294418f6b555636b18a98304
roberto <robertoweller78@gmail.com>
2020-04-19 12:00:18 -0300

    Textura de balão auto dimensionável
    Sua função é se adaptar ao tamanho da fala, está em modo teste ainda

--- testes/test_mani_text.py
+++ testes/test_mani_text.py
@@ -6,19 +6,19 @@ class TestTexto(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # Se tiver menos que 3 caracters vai ter um tamanho unico
-        if len(self.ids.bot.text) < 3:
+        if len(self.ids.bot.text) <= 3:
             # Botão do meio se adapta à quantidades de letras (só na largura do botão)
-            self.ids.bot.text_size[0] = 20*3-30
-            self.ids.box.width = f'{20*3}px'
+            self.ids.bot.text_size[0] = 20*3.5-30
+            self.ids.box.width = f'{20*3.5}px'
         else:
             self.ids.bot.text_size[0] = 20*len(self.ids.bot.text)-30
             self.ids.box.width = f'{20*len(self.ids.bot.text)}px'
 
         # Como se outro widget tivesse sido adicionado
-        if len(self.ids.bot1.text) < 3:
+        if len(self.ids.bot1.text) <= 3:
             # Botão do meio se adapta à quantidades de letras (só na largura do botão)
-            self.ids.bot1.text_size[0] = 20*3-30
-            self.ids.box1.width = f'{20*3}px'
+            self.ids.bot1.text_size[0] = 20*3.5-30
+            self.ids.box1.width = f'{20*3.5}px'
         else:
             self.ids.bot1.text_size[0] = 20*len(self.ids.bot1.text)-30
             self.ids.box1.width = f'{20*len(self.ids.bot1.text)}px'
