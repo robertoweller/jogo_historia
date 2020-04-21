320d6065c7c733e7d483e0441229262a85e2c193
roberto <robertoweller78@gmail.com>
2020-04-21 07:36:17 -0300

    Exemplo com balões adapitaveis
    Ainda não desabilitei a imagem

--- testes/lab_test.py
+++ testes/lab_test.py
@@ -1,20 +1,23 @@
 from kivy.app import App
+from kivy.metrics import sp
+from kivy.properties import NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.metrics import sp
 
 
 class Mensagem(BoxLayout):
-    def __init__(self,tarefas,**kwargs):
+    def __init__(self, tarefas, **kwargs):
         super().__init__(**kwargs)
+        
         for tarefa in tarefas:
             self.ids.box.add_widget(Definir(tarefa))
 
 
 class Definir(BoxLayout):
-    def __init__(self, texto='',**kwargs):
+    def __init__(self, texto='', **kwargs):
         super().__init__(**kwargs)
         self.orientation = 'horizontal'
+        self.spacing = 10
         self.add_widget(
             Button(
                 size_hint = (None, None),
@@ -27,6 +30,7 @@ class Definir(BoxLayout):
 
 
 class Adaptavel(Button):
+    largura = NumericProperty(400)
     def __init__(self, texto, **kwargs):
         super().__init__(**kwargs)
         self.size_hint = (None, None)
@@ -34,16 +38,20 @@ class Adaptavel(Button):
         self.text = texto
         self.background_normal = 'balao_rosa.png'
         self.background_down = 'balao_rosa.png'
-
+    # Teste para entender os eventos
+    def on_largura(self, *args):
+        print('Mudou')
     def on_size(self, *args):
         self.text_size = (self.width - sp(30), None)
 
     def on_texture_size(self, *args):
+        print('Mudou')
         self.size = self.texture_size
         self.height += sp(20)
         self.width += sp(30)
-        if self.width > 550:
-            self.size_hint = (1, None)
+        if self.width > self.largura:
+            self.width = self.largura
+
             # self.ids.imagem.background_normal = None
             # self.ids.imagem.background_down = None
 
@@ -51,11 +59,11 @@ class Adaptavel(Button):
 class Test(App):
     def build(self):
         return Mensagem([
-            'Coisas para fazer hoje :)',
+            'Coisas para fazer hoje :))))))',
             'Comprar pão',
             'Compra mascara',
             ':)',
-            'sedjfjfgofoajfoasofaosjfsoajfosajfjsafjasofjaosjfoasjfhfhaahoiashdoidoiashdoihsadoisahdois'
+            'sedjfjfgofoajf'
         ])
 
 Test().run()
