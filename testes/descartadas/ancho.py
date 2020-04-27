d1da536b44da0ace9f85d29b12f5b72782671a5a
roberto <robertoweller78@gmail.com>
2020-04-27 18:05:03 -0300

    Teste para resolver
    Esse é um teste para ver se funciona a idéa de empurrar os widgets para o outro lado, para simular a conversa do jogador

--- testes/descartadas/ancho.py
+++ testes/descartadas/ancho.py
@@ -10,11 +10,32 @@ class Mensagem(BoxLayout):
     def __init__(self, tarefas, **kwargs):
         super().__init__(**kwargs)
         for tarefa in tarefas:
-            self.ids.box.add_widget(Button(text=tarefa,font_size=30,size_hint_y=None,height=200))
+            self.ids.box.add_widget(Widgets(tarefa))
                
+
+class Widgets(BoxLayout):
+    def __init__(self, texto='', **kwargs):
+        super().__init__(**kwargs)
+        
+        # self.add_widget(Label(size_hint=(1, 1))
+        self.orientation = 'horizontal'
+        self.size_hint_y = None
+        self.size_hint_x = None
+        self.height = 100
+        self.width = 100
+        # A idéia é por um label para empurrar para lá -> os botões
+        self.add_widget(Button(
+                text=texto, 
+                font_size=30, 
+                size_hint_y=None,
+                height=100
+            ))
+        # self.add_widget(Label(size_hint=(1, 1)))
+
+
 
 class AssimAnchor(App):
     def build(self):
-        return Mensagem(['A', 'B'])
+        return Mensagem(['A', 'B', 'C'])
 
 AssimAnchor().run()
