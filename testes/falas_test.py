089b63c9254d027c3a165b300a5715744e8ab88f
roberto <robertoweller78@gmail.com>
2020-04-26 22:34:21 -0300

    Novo designe e balão
    Balão com novo designe adicionado ao projeto teste.

--- testes/falas_test.py
+++ testes/falas_test.py
@@ -33,7 +33,8 @@ class Mensagem(BoxLayout):
     def __init__(self, texto, **kwargs):
         super().__init__(**kwargs)
         self.con = 0
-        
+        self.eu_balao = 'baloes/eu_baixo.png'
+        self.cayla_balao = 'baloes/cay_baixo.png'
         # Se for True, não vai adicionar a imagem na quarta vez
         # Se for False, vai adicionar balão  imagem normalmente
         self.img = False
@@ -52,44 +53,59 @@ class Mensagem(BoxLayout):
                     # Quando chega à 4 adiciona apenas o balão, e volta a contar
                     self.ids.box.add_widget(Adaptavel(
                         texto = tarefa,
-                        balao = 'cayla_rosa.png'))
+                        balao = self.cayla_balao))
                     self.con = 0
             else:
                 # self.ids.box.add_widget(CaylaFala(tarefa))
 
                 self.ids.box.add_widget(PersonFala(
+                    eu=True,
                     texto=tarefa, 
                     person='cayla_rosa.png',
-                    balao = 'baloes/cay_baixo.png'
+                    balao = self.eu_balao
                     ))
 
 # Personagem [Personagem + adiciona(balão)]
 class PersonFala(BoxLayout):
 
-    def __init__(self, texto='', person='', balao ='', **kwargs):
+    def __init__(self, texto='', person='', balao ='', eu=False, **kwargs):
 
         super().__init__(**kwargs)
         self.orientation = 'horizontal'
         self.spacing = 15
         self.padding= (0, 10, 0, 10)
         self.top = 1
-        
-        # Personagem
-        self.add_widget(
-        Button(
-                size_hint = (None, None),
-                pos_hint={'center':self.top},
-                border=(0, 0, 0, 0),
-                background_normal = person,
-                background_down = person
-            ))
-        
-        # Adiciona o balão do desse lado ->
-        self.add_widget(
-            Adaptavel(
-                texto=texto,
-                balao=balao
+        if eu:
+            print('eu')
+            self.add_widget(
+                Adaptavel(
+                    texto=texto,
+                    balao=balao
+                    ))
+            self.add_widget(
+                Button(
+                    size_hint = (None, None),
+                    pos_hint={'center':self.top},
+                    border=(0, 0, 0, 0),
+                    background_normal = person,
+                    background_down = person))
+        else:
+            # Personagem da Cayla
+            self.add_widget(
+            Button(
+                    size_hint = (None, None),
+                    pos_hint={'center':self.top},
+                    border=(0, 0, 0, 0),
+                    background_normal = person,
+                    background_down = person
                 ))
+            
+            # Adiciona o balão do desse lado ->
+            self.add_widget(
+                Adaptavel(
+                    texto=texto,
+                    balao=balao
+                    ))
 
 
 # Balão da Cayla
@@ -101,8 +117,8 @@ class Adaptavel(Button):
         self.font_size = sp(30)
         self.text = texto
         self.pos_hint = {'bottom': 1}
-        self.background_normal = 'baloes/cay_baixo.png'
-        self.background_down = 'baloes/cay_baixo.png'
+        self.background_normal = balao
+        self.background_down = balao
         
     def on_size(self, *args):
         self.text_size = (self.width - sp(30), None)
