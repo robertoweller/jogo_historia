b17d83a86d6525ed0362f2af9704fe33cf666fa2
roberto <robertoweller78@gmail.com>
2020-04-27 20:13:15 -0300

    Quase entendendo AchorLayout
    Entendi mais ou menos como funciona, mas ainda não me resolve o problema direito

--- testes/descartadas/ancho.py
+++ testes/descartadas/ancho.py
@@ -2,9 +2,10 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.anchorlayout import AnchorLayout
+
 
 # Teste para entender o AnchorLayout
-
 class Mensagem(BoxLayout):
     """return: Lista de Button"""
     def __init__(self, tarefas, **kwargs):
@@ -13,23 +14,38 @@ class Mensagem(BoxLayout):
             self.ids.box.add_widget(Widgets(tarefa))
                
 
-class Widgets(BoxLayout):
+class Widgets(AnchorLayout):
     def __init__(self, texto='', **kwargs):
         super().__init__(**kwargs)
+        self.anchor_x='right'
+        self.anchor_y='bottom'
+        # AnchorLayout vai ficar dentro do BoxLayout
         
+
+        # Se não der certo vou usar o label para empurrar ->
         # self.add_widget(Label(size_hint=(1, 1))
-        self.orientation = 'horizontal'
+
+        #self.orientation = 'horizontal'
         self.size_hint_y = None
-        self.size_hint_x = None
-        self.height = 100
-        self.width = 100
-        # A idéia é por um label para empurrar para lá -> os botões
+        #self.size_hint_x = None
+        
+        
         self.add_widget(Button(
                 text=texto, 
                 font_size=30, 
-                size_hint_y=None,
-                height=100
+                size_hint=(None,None),
+                height=100,
+                width=100,
+                
+
             ))
+        self.add_widget(Label(
+            text= 'AAAA', 
+            size_hint=(None, None), 
+            height=100, 
+            width=100
+            ))
+
         # self.add_widget(Label(size_hint=(1, 1)))
 
 
